ed7b246d6d6401bcb410aa7917678c44f7c70f4f
harshb25 <71058438+harshb25@users.noreply.github.com>
2020-11-25 19:51:11 +0530

    Add files via upload
    Added draw and setup functions

---
+++ sketch .js
@@ -0,0 +1,34 @@
+
+let graphCalculator;
+let drawingMachine;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight).parent('p5');
+  drawingMachine = new DrawingMachine();
+
+
+  document.querySelector('#calculate').addEventListener('click', () => {
+
+    let startPoint = document.querySelector('#from').value;
+    let endPoint = document.querySelector('#to').value;
+    drawingMachine.calculateRoute(startPoint, endPoint);
+
+  });
+
+  document.querySelector('#generate').addEventListener('click', () => {
+
+    drawingMachine.openGraphToCopy();
+
+  });
+}
+
+function draw() {
+  background(255);
+
+  drawingMachine.drawGraph();
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
